11d9b6e7491e61e542a30685c9a119ba305da0f3
saikumar <saikumarreddybarda@gmail.com>
2025-02-19 22:43:32 -0600

    My first  commit

---
+++ src/main/java/com/example/e_comPro/EComProApplication.java
@@ -0,0 +1,14 @@
+package com.example.e_comPro;
+
+import org.aspectj.lang.annotation.Around;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComProApplication.class, args);
+	}
+
+}
