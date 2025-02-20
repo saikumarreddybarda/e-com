2045ec0be14ebfff5bec3a1018e66082487d3e76
saikumar <saikumarreddybarda@gmail.com>
2025-02-19 23:19:09 -0600

    My second  commit

--- src/main/java/com/example/e_comPro/EComProApplication.java
+++ src/main/java/com/example/e_comPro/EComProApplication.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.annotation.Around;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //spring boot app
 public class EComProApplication {
 
 	public static void main(String[] args) {
